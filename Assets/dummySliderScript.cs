033ca4db04102e2961abd13c56e0cd02d075de15
sebastiaan van Rijn <sebastiaan.vanrijn@student.hu.nl>
2018-06-01 15:47:06 +0200

    redesigned UI for recording ui | added dummyscript for slider and frame counter

---
+++ Assets/dummySliderScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dummySliderScript : MonoBehaviour {
+
+    public TMP_Text frameText;
+    public Slider frameSlider;
+    public int maxFrame = 80;
+
+    private void Start()
+    {
+        frameSlider.maxValue = maxFrame;
+        frameSlider.value = maxFrame;
+    }
+
+    public void OnSliderEdit(float input)
+    {
+        frameText.text = "frame " + input.ToString() + "/" + maxFrame.ToString();
+    }
+	
+}
